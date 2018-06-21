7801a227cea5361e1a5d8c010aa0b24e468e41ee
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-21 18:20:17 +0100

    Generalize importing assets

--- lib/import.rb
+++ lib/import.rb
@@ -13,96 +13,89 @@ class Import
     # XXX Reset everything on each run for now.
     `rails db:reset`
 
-    import_chassis
-    import_servers
-    import_network_adapters
+    servers_to_chassis = import_chassis
+    network_adapters_to_servers = import_servers(servers_to_chassis)
+    import_network_adapters(network_adapters_to_servers)
     import_nodes
   end
 
   private
 
-  attr_reader :ssh_connection,
-    :server_chassis_relationships,
-    :network_adapter_server_relationships
+  attr_reader :ssh_connection
 
   def initialize(ssh_connection)
     @ssh_connection = ssh_connection
   end
 
   def import_chassis
-    all_chassis_data = metal_view('assets.chassis')
-
-    STDERR.puts "Found #{all_chassis_data.length} chassis"
-
-    chassis = all_chassis_data.map do |data|
-      name = asset_name(data)
-      STDERR.puts "Importing chassis #{name}..."
-      chassis = Chassis.create!(name: name, data: data)
-    end
-
-    # Create Server name -> Chassis name hash
-    @server_chassis_relationships = chassis.flat_map do |a_chassis|
-      server_names = a_chassis.data['servers'].flat_map do |reference|
-        # Reference is an @WilliamMcCumstie-style Metalware asset reference,
-        # e.g. `^rack1-r630-chassis-780-server1`; we just want the name of the
-        # referenced asset.
-        reference.gsub(/^\^/, '')
-      end
-
-      server_names.zip([a_chassis.name] * server_names.length)
-    end.to_h
-
-    STDERR.puts "Found these server-chassis relationships:"
-    STDERR.puts JSON.pretty_generate(server_chassis_relationships).gsub(/^/, '    ')
+    import_assets_of_type(Chassis, child_class: Server)
   end
 
-  def import_servers
-    all_server_data = metal_view('assets.servers')
+  def import_servers(servers_to_chassis)
+    import_assets_of_type(
+      Server,
+      parent_class: Chassis,
+      parents_map: servers_to_chassis,
+      child_class: NetworkAdapter
+    )
+  end
 
-    STDERR.puts "Found #{all_server_data.length} servers"
+  def import_network_adapters(network_adapters_to_servers)
+    import_assets_of_type(
+      NetworkAdapter,
+      parent_class: Server,
+      parents_map: network_adapters_to_servers
+    )
+  end
 
-    servers = all_server_data.map do |data|
+  def import_assets_of_type(klass, parent_class: nil, parents_map: nil, child_class: nil)
+    asset_type = klass.to_s.underscore
+    human_asset_type = asset_type.humanize(capitalize: false)
+
+    parent_type = parent_class.to_s.underscore
+    child_type = child_class.to_s.underscore
+
+    STDERR.puts "Importing #{human_asset_type.pluralize}"
+
+    all_data = metal_view("assets.#{asset_type.pluralize}")
+    STDERR.puts "Found #{all_data.length} #{human_asset_type.pluralize}"
+
+    assets = all_data.map do |data|
       name = asset_name(data)
-      STDERR.puts "Importing server #{name}"
+      STDERR.puts "Importing #{human_asset_type} #{name}"
 
-      chassis_name = server_chassis_relationships.fetch(name)
-      chassis = Chassis.find_by_name!(chassis_name)
-      Server.create!(name: name, data: data, chassis: chassis)
+      asset_attributes = {name: name, data: data}
+      if parent_class
+        parent_name = parents_map.fetch(name)
+        parent = parent_class.find_by_name!(parent_name)
+        asset_attributes.merge!(parent_type => parent)
+      end
+
+      klass.create!(asset_attributes)
     rescue KeyError => ex
-      STDERR.puts "WARNING: Server #{name} has no chassis, skipping: #{ex.message}"
+      STDERR.puts "WARNING: #{human_asset_type} #{name} has no #{parent_class}, skipping: #{ex.message}"
     end.compact
 
-    # Create Network adapter name -> Server name hash
-    @network_adapter_server_relationships = servers.flat_map do |server|
-      network_adapter_names = server.data['network_adapters'].flat_map do |reference|
+    # Exit early unless there's a child class to find relationships with.
+    return unless child_class
+
+    # Create Child name -> Asset name hash
+    relationships = assets.flat_map do |asset|
+      children_names = asset.data[child_type.pluralize].flat_map do |reference|
         # Reference is an @WilliamMcCumstie-style Metalware asset reference,
         # e.g. `^rack1-r630-chassis-780-server1`; we just want the name of the
         # referenced asset.
         reference.gsub(/^\^/, '')
       end
 
-      network_adapter_names.zip([server.name] * network_adapter_names.length)
+      children_names.zip([asset.name] * children_names.length)
     end.to_h
 
-    STDERR.puts "Found these network adapter-server relationships:"
-    STDERR.puts JSON.pretty_generate(network_adapter_server_relationships).gsub(/^/, '    ')
-  end
+    STDERR.puts "Found these #{child_type.humanize(capitalize: false)}-#{human_asset_type} relationships:"
+    STDERR.puts JSON.pretty_generate(relationships).gsub(/^/, '    ')
 
-  def import_network_adapters
-    all_network_adapter_data = metal_view('assets.network_adapters')
-
-    STDERR.puts "Found #{all_network_adapter_data.length} network adapters"
-
-    all_network_adapter_data.map do |data|
-      name = asset_name(data)
-      STDERR.puts "Importing network adapter #{name}"
-
-      server_name = network_adapter_server_relationships.fetch(name)
-      server = Server.find_by_name!(server_name)
-      NetworkAdapter.create!(name: name, data: data, server: server)
-    rescue KeyError => ex
-      STDERR.puts "WARNING: Network adapter #{name} has no server, skipping: #{ex.message}"
-    end
+    # Return the child-asset relationships to use when importing children.
+    relationships
   end
 
   def import_nodes
