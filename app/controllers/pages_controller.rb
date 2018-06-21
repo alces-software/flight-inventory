baccb5bbcfa92894239c98a8fdff9be1a3c95b04
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-21 13:15:01 +0100

    Generalize forming asset data for diagram

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
+  ASSET_TYPES =[
+    Chassis,
+    Node,
+    Server,
+  ]
+
   def root
-    @asset_data = {
-      chassis: Chassis.all,
-      servers: Server.all,
-      nodes: Node.all,
-    }
+    @asset_data = ASSET_TYPES.map do |asset_class|
+      [
+        asset_class.to_s.downcase.pluralize,
+        asset_class.all
+      ]
+    end.to_h
   end
 end
