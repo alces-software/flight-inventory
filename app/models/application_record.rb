6775009ee7e5b53046bca683d5322dadc62b2cfd
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-22 15:18:04 +0100

    Display model for various assets in view

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,12 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   default_scope { order(:name) }
+
+  # XXX Should probably be in concern since only applicable to physical assets.
+  def full_model
+    [
+      data['manufacturer'],
+      data['model'],
+    ].join(' ')
+  end
 end
