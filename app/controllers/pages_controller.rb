8c30f6814cc814fc287433dde3210d6dce576027
Bob Whitelock <bob.whitelock1@gmail.com>
2018-06-22 14:53:41 +0100

    Sketch initial possibly better approach to view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,18 +1,5 @@
 class PagesController < ApplicationController
-  ASSET_TYPES =[
-    Chassis,
-    NetworkAdapter,
-    Node,
-    Psu,
-    Server,
-  ]
-
   def root
-    @asset_data = ASSET_TYPES.map do |asset_class|
-      [
-        asset_class.to_s.camelize(:lower).pluralize,
-        asset_class.all
-      ]
-    end.to_h
+    @chassis = Chassis.all
   end
 end
