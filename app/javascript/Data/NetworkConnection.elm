e98fcc5644097422052323e5a5b4d2dccd59bf3a
Bob Whitelock <bob.whitelock1@gmail.com>
2018-07-17 11:13:21 +0100

    Add and decode NetworkConnection nodeId field

--- app/javascript/Data/NetworkConnection.elm
+++ app/javascript/Data/NetworkConnection.elm
@@ -4,6 +4,7 @@ import Data.Asset as Asset
 import Data.Network as Network
 import Data.NetworkAdapterPort as NetworkAdapterPort
 import Data.NetworkSwitch as NetworkSwitch
+import Data.Node as Node
 import Json.Decode as D
 import Json.Decode.Pipeline as P
 
@@ -12,6 +13,7 @@ type alias NetworkConnection =
     { networkId : Network.Id
     , networkAdapterPortId : NetworkAdapterPort.Id
     , networkSwitchId : NetworkSwitch.Id
+    , nodeId : Maybe Node.Id
     }
 
 
@@ -21,3 +23,4 @@ decoder =
         |> P.required "network_id" Asset.idDecoder
         |> P.required "network_adapter_port_id" Asset.idDecoder
         |> P.required "network_switch_id" Asset.idDecoder
+        |> P.required "node_id" (D.nullable Asset.idDecoder)
