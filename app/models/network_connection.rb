774ee44fdd25f319284a81fa1acf6b481ca205d2
Bob Whitelock <bob.whitelock1@gmail.com>
2018-07-20 17:54:11 +0100

    Remove no longer needed default scope
    We now sort everything as needed within the Elm app, so no need to
    always attempt to order things by name when we select them. Particularly
    worth removing this as some models do not have a `name` field, so have
    to also `unscope` this in these.

--- app/models/network_connection.rb
+++ app/models/network_connection.rb
@@ -7,9 +7,4 @@ class NetworkConnection < ApplicationRecord
   belongs_to :network_adapter_port
   belongs_to :network_switch
   belongs_to :node, optional: true
-
-  # XXX Remove default scope from ApplicationRecord to order by non-existent
-  # (for this model) `name` field - should probably only define that where it's
-  # relevant.
-  default_scope { unscope(:order) }
 end
